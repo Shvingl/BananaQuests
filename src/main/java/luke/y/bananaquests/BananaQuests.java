e58083e8520734b87f2c55d655a28fc0c39c6899
Luke Y <lukas.schwan3@gmail.com>
2024-08-27 15:00:40 +0200

    zdaer

--- src/main/java/luke/y/bananaquests/BananaQuests.java
+++ src/main/java/luke/y/bananaquests/BananaQuests.java
@@ -1,8 +1,15 @@
 package luke.y.bananaquests;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public final class BananaQuests extends JavaPlugin {
+
+    private Map<Player, ArrayList<ActiveQuest>> activeQuestsMap;
 
     @Override
     public void onEnable() {
@@ -13,5 +20,12 @@ public final class BananaQuests extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            savePlayersQuests(player);
+        }
+    }
+
+    public void savePlayersQuests(Player player) {
+
     }
 }
