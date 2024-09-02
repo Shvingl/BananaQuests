a3e844d3628dc515f4c86218df1e3d4c8026b17a
Luke Y <lukas.schwan3@gmail.com>
2024-09-02 13:42:02 +0200

    Nove metody v ActiveQuest

--- src/main/java/luke/y/bananaquests/ActiveQuest.java
+++ src/main/java/luke/y/bananaquests/ActiveQuest.java
@@ -1,32 +1,38 @@
 package luke.y.bananaquests;
 
+import luke.y.bananaquests.objective.KillMythicMobObjective;
 import luke.y.bananaquests.objective.QuestObjective;
 
 import java.util.ArrayList;
 
 /**
  * This class represents an ongoin/finished quest.
- * Belongs to a specific player's quest list.
+ * Must belong to a specific online player's quest list.
  */
 public class ActiveQuest {
     private int stage;
     private ArrayList<QuestObjective> currentObjectives;
 
-    public void ActiveQuest(String id, int stage, ArrayList<QuestObjective> currentObjectives) {
+    public ActiveQuest(String id, int stage, ArrayList<QuestObjective> currentObjectives) {
         this.stage = stage;
         this.currentObjectives = currentObjectives;
     }
 
-    public void checkObjectives() {
+    private boolean allObjectivesFinished() {
         int finishedObjectives = 0;
         for (QuestObjective objective : currentObjectives) {
-            if (objective.isFinished() == true)
+            if (objective.isFinished())
                 finishedObjectives++;
         }
         if (finishedObjectives >= currentObjectives.size()) {
-            //All objectives are finished -> move to the next stage
-            currentObjectives.clear();
-            stage++;
+            return true;
         }
+        return false;
+    }
+
+    private void moveToNextStage() {
+        stage++;
+        currentObjectives.clear();
+        //currentObjectives.add();
     }
 }
--- src/main/java/luke/y/bananaquests/BananaQuests.java
+++ src/main/java/luke/y/bananaquests/BananaQuests.java
@@ -1,23 +1,29 @@
 package luke.y.bananaquests;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public final class BananaQuests extends JavaPlugin {
 
     ArrayList<String> validQuests;
-    private Map<Player, ArrayList<ActiveQuest>> activeQuestsMap;
+    private HashMap<Player, ArrayList<ActiveQuest>> activeQuestsMap;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        activeQuestsMap = new HashMap<>();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            loadPlayersQuests(player);
+        }
         //Projit všechny quest .yml soubory a dát je do validQuests
         for (File file : new File(this.getDataFolder().getAbsolutePath() + File.separator + "quests").listFiles()) {
             YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
@@ -33,7 +39,34 @@ public final class BananaQuests extends JavaPlugin {
         }
     }
 
+    /**
+     * Save data to player.yml
+     */
     public void savePlayersQuests(Player player) {
 
+    }
+
+    /**
+     * Method to be called on each player join.
+     * Loads player's active quests from the .yml file to the plugin's
+     * player quest map.
+     */
+    public void loadPlayersQuests(Player player) {
+        File playerFile = new File(this.getDataFolder().getAbsoluteFile() + File.separator + "playerdata" + File.separator + player.getName() + ".yml");
+        if (!playerFile.exists()) {
+            YamlConfiguration empty = new YamlConfiguration();
+            try {
+                empty.save(playerFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return;
+        }
+        ArrayList<ActiveQuest> questsToAdd = new ArrayList<>();
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(playerFile);
+        for (String questID : config.getConfigurationSection("active").getKeys(false)) {
+            ConfigurationSection questsSection = config.getConfigurationSection("active." + questID);
+            //questsToAdd.add(new ActiveQuest(questID, questsSection.getInt("stage"), ))
+        }
     }
 }
