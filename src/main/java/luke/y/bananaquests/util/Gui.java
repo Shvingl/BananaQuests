c23508bca06d08cc51a1b980501191ab5de90865
Luke Y <lukas.schwan3@gmail.com>
2024-09-05 22:13:10 +0200

    Quests GUI shell

--- src/main/java/luke/y/bananaquests/util/Gui.java
+++ src/main/java/luke/y/bananaquests/util/Gui.java
@@ -3,6 +3,7 @@ package luke.y.bananaquests.util;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -10,12 +11,31 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class Gui {
 
+    public static final String activeTitle = "Aktivní Questy";
+    public static final String finishedTitle = "Dokončené Questy";
+    public static final String unstartedTitle = "Nezapočaté Questy";
+
     public static void openActiveQuestsGUI(Player player) {
+        Inventory inventory = Bukkit.createInventory(player, 9*5, activeTitle);
+        inventory.setContents(createShell(player).getStorageContents());
+        ItemStack activeButton = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
+        ItemMeta activeButtonMeta = activeButton.getItemMeta();
+        activeButtonMeta.setDisplayName(ChatColor.GREEN + activeTitle);
+        activeButton.setItemMeta(activeButtonMeta);
+        inventory.setItem(9*2-1, activeButton);
+        player.openInventory(inventory);
+    }
+
+    public static void openFinishedQuestsGUI(Player player) {
+        player.openInventory(createShell(player));
+    }
+
+    public static void openUnstartedQuestsGUI(Player player) {
         player.openInventory(createShell(player));
     }
 
     private static Inventory createShell(Player player) {
-        Inventory gui = Bukkit.createInventory(player, 9*5, ChatColor.GRAY + "Quests");
+        Inventory gui = Bukkit.createInventory(player, 9*5);
         ItemStack emptyButton = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
         ItemMeta emptyButtonMeta = emptyButton.getItemMeta();
         emptyButtonMeta.setDisplayName("");
